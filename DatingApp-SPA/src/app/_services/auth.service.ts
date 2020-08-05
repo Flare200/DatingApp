4d8708451ac199e871faff9afea8929935edbebe
Elijah Ratner <firebird567@cox.net>
2020-08-05 11:15:25 -0700

    Added Routing to the Application
    Setup routing with the Angular router
    Protected the routes with a route guard so no unauthorized users could access them

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -22,7 +22,6 @@ login(model: any) {
       if (user) {
         localStorage.setItem('token', user.token);
         this.decodedToken = this.jwtHelper.decodeToken(user.token);
-        console.log(this.decodedToken);
       }
     })
   );
