7d80d357b19b5626b4906830791f14cee0c449ce
Elijah Ratner <firebird567@cox.net>
2020-08-15 11:01:39 -0700

    Fixed an Issue with the Routes
    Noticed that the default route would not redirect to home if it was accessed first

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -14,6 +14,7 @@ import { ListsResolver } from './_resolvers/lists.resolver';
 import { MessagesResolver } from './_resolvers/messages.resolver';
 
 export const appRoutes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {
         path: '',
